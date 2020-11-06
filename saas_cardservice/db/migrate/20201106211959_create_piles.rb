b7855c2ebf66e69e8089153ea712d19f74e84f50
DTMcNamara <DTMcNamara13@gmail.com>
2020-11-06 15:47:50 -0600

    refactored from deck to generalized piles

---
+++ saas_cardservice/db/migrate/20201106211959_create_piles.rb
@@ -0,0 +1,8 @@
+class CreateDeck < ActiveRecord::Migration[6.0]
+  def change
+    create_table :piles do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- saas_cardservice/db/schema.rb
+++ saas_cardservice/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_162715) do
+ActiveRecord::Schema.define(version: 2020_11_06_211959) do
 
   create_table "cards", force: :cascade do |t|
     t.string "suit"
     t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "piles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
