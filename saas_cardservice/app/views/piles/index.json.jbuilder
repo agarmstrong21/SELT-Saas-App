fab80ce5027949dbeba4de09ebcfa764cafc4a46
Addison Armstrong <55289031+agarmstrong21@users.noreply.github.com>
2020-11-06 20:12:10 -0600

    Revert "ADD: Create Piles, and Create Decks ( Branch #175530633 )"

--- saas_cardservice/app/views/piles/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @piles, partial: "piles/pile", as: :pile
--- saas_cardservice/config/routes.rb
+++ saas_cardservice/config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  resources :piles
-  get 'piles/new'
-  get 'piles/show'
-  get 'piles/index'
-  get 'piles/create'
-  get 'piles/update'
-  get 'piles/destroy'
-  get 'piles/new'
-  get 'piles/show'
-  get 'piles/index'
   resources :cards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
