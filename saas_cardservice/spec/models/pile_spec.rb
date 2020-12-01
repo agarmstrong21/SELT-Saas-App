cf067545015edd026c0690d3e70ad70bca5bdcf4
Kayla <kayla.mf6@gmail.com>
2020-11-30 20:42:18 -0600

    RSpec added. Shuffle backend complete.

--- saas_cardservice/spec/models/pile_spec.rb
+++ saas_cardservice/spec/models/pile_spec.rb
@@ -71,9 +71,9 @@ RSpec.describe Pile, type: :model do
   describe 'moving cards' do
     context 'create a deck and a hand' do
       it 'should have 1 card in hand after first draw' do
-        Pile.create_hand!(Name:'Hand1', pile_type:'hand')
+        Pile.create_hand!(Name: 'Hand1', pile_type: 'hand')
         hand = Pile.find_by(Name: 'Hand1')
-        Pile.create_deck!(Name:'Deck1', pile_type:'deck')
+        Pile.create_deck!(Name: 'Deck1', pile_type: 'deck')
         deck = Pile.find_by(Name: 'Deck1')
         deck.move_card(hand.id)
         handlength = hand.cards.length
@@ -87,6 +87,27 @@ RSpec.describe Pile, type: :model do
         deck.move_card(hand.id)
         decklength = deck.cards.length
         expect(decklength).to equal(51)
+      end
+    end
+  end
+  describe 'shuffle pile' do
+    context 'shuffle deck' do
+      it 'should move the cards around randomly' do
+        Pile.create_deck!(Name: 'Deck', pile_type: 'deck')
+        deck = Pile.find_by(Name: 'Deck')
+        deck.shuffle_pile
+        possible_suits = ['Diamonds','Spades','Hearts','Clubs']
+        possible_values = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King']
+        spot = 0
+        is_changed = false
+        possible_values.each do |value|
+          possible_suits.each do |suit|
+            card = Card.where(pile: deck).where(position: spot)
+            is_changed = true unless card.pluck(:value).eql?(value) && card.pluck(:suit).eql?(suit)
+            spot += 1
+          end
+        end
+        expect(is_changed).to be_truthy
       end
     end
   end
