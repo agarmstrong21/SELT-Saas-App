972a597a74ebc6a874d8868448b9ee5e73a14af5
DTMcNamara <DTMcNamara13@gmail.com>
2020-12-03 17:37:59 -0600

    ADD: users migration, session token generation, new user sign up, redirected logins

---
+++ saas_cardservice/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+
+class SessionsController < ApplicationController
+
+  def session_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+
+  def create
+    @user = User.find_by user_id: session_params[:user_id], email: session_params[:email]
+    if @user.nil? || session_params[:user_id].nil? || session_params[:email].nil?
+      flash[:notice] = "Sorry but that user id/email is invalid"
+      #redirect_to login_path
+    else
+      session[:session_token] = @user.session_token
+      flash[:notice] = "Welcome #{User.user_id(session_params)}"
+      #redirect_to movies_path
+    end
+  end
+
+  def new
+  end
+
+  def destroy
+    session[:session_token] = nil
+    flash[:notice] = "Logged Out"
+    #redirect_to movies_path
+  end
+
+end
---
+++ saas_cardservice/app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by user_id: user_params[:user_id]
+    if !@user.nil?
+      flash[:notice] = "Sorry but that user id is taken"
+      #redirect_to new_user_path
+    else
+      user_params[:session_token] = SecureRandom.base64
+      create_user!(user_params)
+      flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
+      #redirect_to login_path
+    end
+  end
+
+  def create_user!(user_params)
+    user_params[:session_token] = SecureRandom.base64
+    @user = User.create!(user_params)
+  end
+end
