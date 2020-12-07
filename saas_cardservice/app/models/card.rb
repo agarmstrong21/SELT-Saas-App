bffd9e4fb0e9d9f026567be425d5a96c6080e3f0
Jason Rathjen <jasontrathjen@gmail.com>
2020-12-07 17:35:19 -0600

    FIX: Card creation and DB
    Cards now are created with their associated image automatically. Also fixed the DB values to make code more efficient and cleaned up some styling in the code. This commit will make all cards appear with their correct image.

--- saas_cardservice/app/models/card.rb
+++ saas_cardservice/app/models/card.rb
@@ -3,77 +3,17 @@ class Card < ApplicationRecord
 
   # Creating Card method that will create a card with a suit, value, and pile it belongs to.
   def self.create_card!(i)
-    possibleSuits = ["Diamonds","Spades","Hearts","Clubs"]
-    possibleValues = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-    isSuit = false
-    isValue = false
-    isSuit = true if possibleSuits.include? i[:suit]
-    isValue = true if possibleValues.include? i[:value]
-    if isValue and isSuit
+    possible_suits = %w[D S H C]
+    possible_values = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+    is_suit = false
+    is_value = false
+    is_suit = true if possible_suits.include? i[:suit]
+    is_value = true if possible_values.include? i[:value]
+
+    if is_value && is_suit
       cards = Card.where(pile_id: i[:pile].id)
       i[:position] = cards.length
-      case i[:value]
-      when 'Ace'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = "#{i[:value]}D.png"
-        elsif i[:suit].equal?('Spades')
-          i[:image] = 'AS.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = 'AH.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = 'AC.png'
-        end
-      when 'Two'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = '2D.png'
-        elsif i[:suit].equal?('Spades')
-          i[:image] = '2S.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = '2H.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = '2C.png'
-        end
-      when 'Three'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = '3D.png'
-        elsif i[:suit].equal?('Spades')
-          i[:image] = '3S.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = '3H.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = '3C.png'
-        end
-      when 'Four'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = '4D.png'
-        elsif i[:suit].equal?('Spades')
-          i[:image] = '4S.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = '4H.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = '4C.png'
-        end
-      when 'Five'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = '5D.png'
-        elsif i[:suit].equal?('Spades')
-          i[:image] = '5S.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = '5H.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = '5C.png'
-        end
-      when 'Six'
-        if i[:suit].equal?('Diamonds')
-          i[:image] = '6D.png'
-        elsif i[:suit].equal?('Spades')
-          i[:image] = '6S.png'
-        elsif i[:suit].equal?('Hearts')
-          i[:image] = '6H.png'
-        elsif i[:suit].equal?('Clubs')
-          i[:image] = '6C.png'
-        end
-      end
+      i[:image] = "#{i[:value]}#{i[:suit]}.png"
       Card.create!(i)
     end
   end
